826134e0187389679a42be9d1312623912f3e2b8
Fabulous_Arcadia <zhangzs21@mails.tsinghua.edu.cn>
2025-11-30 19:13:08 +0100

    ci test by pytest

--- python/tests/compare/compare_regression.py
+++ python/tests/compare/compare_regression.py
@@ -5,7 +5,7 @@ Compares test_output/ against test_reference/
 """
 
 import sys
-from compare_all_results import compare_test_results
+from python.tests.compare.compare_all_results import compare_test_results
 
 if __name__ == '__main__':
     sys.exit(compare_test_results(mode='regression'))
