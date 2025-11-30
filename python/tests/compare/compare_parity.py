2dd20b580d95d50b5f0956154fb0f042a2ac16e7
Fabulous_Arcadia <zhangzs21@mails.tsinghua.edu.cn>
2025-11-30 19:13:08 +0100

    ci test by pytest

--- python/tests/compare/compare_parity.py
+++ python/tests/compare/compare_parity.py
@@ -4,7 +4,7 @@ Validates that Python implementation matches MATLAB reference.
 """
 
 import sys
-from compare_all_results import compare_test_results
+from python.tests.compare.compare_all_results import compare_test_results
 
 if __name__ == '__main__':
     sys.exit(compare_test_results(mode='parity'))
