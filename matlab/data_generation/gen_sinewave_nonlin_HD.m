7e707160ffdda7f10eedcd58f327fa4feba677b2
Fabulous_Arcadia <zhangzs21@mails.tsinghua.edu.cn>
2025-12-02 17:08:00 +0100

    renewed filepath in matlab files

--- matlab/data_generation/gen_sinewave_nonlin_HD.m
+++ matlab/data_generation/gen_sinewave_nonlin_HD.m
@@ -1,16 +1,16 @@
 %% Generate sinewave with specific HD2 / HD3 distortion levels
 close all; clear; clc; warning("off");
 rng(42);
-
-subFolder = "dataset/aout/sinewave";
+subFolder = fullfile("dataset", "sinewave");
+if ~exist(subFolder, 'dir'), mkdir(subFolder); end
 
 %% Sinewave with specific HD2 / HD3 distortion levels
-HD2_dB_list = -70; % Target HD2 levels in dB
-HD3_dB_list = -65; % Target HD3 levels in dB
+HD2_dB_list = -150; % Target HD2 levels in dB
+HD3_dB_list = -66.19; % Target HD3 levels in dB
 
 N = 2^13;
 Fs = 1e9;
-J = findBin(Fs, 100e6, N);
+J = findBin(Fs, 80e6, N);
 A = 0.499;
 sinewave = A * sin((0:N - 1)*J*2*pi/N); % Base sinewave (zero mean)
 
@@ -29,13 +29,14 @@ for k = 1:length(HD2_dB_list)
         % Generate distorted waveform (zero-mean → nonlinear → add DC)
         % Add small noise for practical simulation
         data = sinewave + coef2 * (sinewave.^2) + coef3 * (sinewave.^3);
-        data = data + 0.5 + randn(1, N) * 1e-6;
+        data = data + 0.5 + randn(1, N) * 1e-4;
 
-        hd2_str = sprintf("HD2_n%ddB", abs(HD2_dB_list(k)));
-        hd3_str = sprintf("HD3_n%ddB", abs(HD3_dB_list(k2)));
+        hd2_str = sprintf("HD2_n%0.0fdB", abs(HD2_dB_list(k)));
+        hd3_str = sprintf("HD3_n%0.0fdB", abs(HD3_dB_list(k2)));
         filename = fullfile(subFolder, sprintf("sinewave_%s_%s.csv", hd2_str, hd3_str));
-        ENoB = specPlot(data,"isplot",0);
         writematrix(data, filename)
+
+        ENoB = specPlot(data,"isplot",0);
         fprintf("  [ENoB = %0.2f] [Save] %s\n", ENoB, filename);
     end
 end
