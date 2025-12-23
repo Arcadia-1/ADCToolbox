77b5eaa63832f9a23d529299ca7514024e8bff4b
many-question <jielu1995@outlook.com>
2025-12-24 01:20:48 +0800

    Changes to be committed: 	modified:   matlab/README.md 	renamed:    matlab/src/ovfchk.m -> matlab/src/bitchk.m 	modified:   matlab/src/legacy/FGCalSine.m 	renamed:    matlab/src/bitact.m -> matlab/src/legacy/bitact.m 	renamed:    matlab/src/bitsweep.m -> matlab/src/legacy/bitsweep.m 	modified:   matlab/src/legacy/overflowChk.m 	modified:   matlab/src/legacy/weightScaling.m 	modified:   matlab/src/plotwgt.m 	modified:   matlab/src/shortcut/errsinv.m 	modified:   matlab/src/toolset_dout.m 	renamed:    matlab/src/wcalsine.m -> matlab/src/wcalsin.m

--- matlab/src/legacy/overflowChk.m
+++ matlab/src/legacy/overflowChk.m
@@ -1,10 +1,10 @@
 function overflowChk(raw_code,weight,OFB)
 %OVERFLOWCHK Check ADC overflow by analyzing bit segment residue distributions (legacy)
 %   DEPRECATED: This function is maintained for backward compatibility.
-%   Please use ovfchk (lowercase) instead.
+%   Please use bitchk instead.
 %
-%   This function is a wrapper that calls ovfchk with the same
-%   functionality. All new code should use ovfchk directly.
+%   This function is a wrapper that calls bitchk with the same
+%   functionality. All new code should use bitchk directly.
 %
 %   Legacy interface:
 %     OVERFLOWCHK(raw_code, weight)
@@ -21,13 +21,13 @@ function overflowChk(raw_code,weight,OFB)
 %   Outputs:
 %     None (displays a visualization plot)
 %
-%   See also: ovfchk
+%   See also: bitchk
 
-    % Call the new ovfchk function with appropriate arguments
+    % Call the new bitchk function with appropriate arguments
     if nargin < 3
-        ovfchk(raw_code, weight);
+        bitchk(raw_code, weight);
     else
-        ovfchk(raw_code, weight, OFB);
+        bitchk(raw_code, weight, OFB);
     end
 
 end
